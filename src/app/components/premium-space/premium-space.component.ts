09718287ca6fb5c84ccb759a1fbc7dbd62faaf54
Cleiton Sampaio <cleiton.nso2019@gmail.com>
2025-04-26 19:20:24 -0300

    criando site

--- src/app/components/premium-space/premium-space.component.ts
+++ src/app/components/premium-space/premium-space.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-premium-space',
-  imports: [],
   templateUrl: './premium-space.component.html',
-  styleUrl: './premium-space.component.scss',
-  standalone: true
+  styleUrls: ['./premium-space.component.scss']
 })
-export class PremiumSpaceComponent {
+export class PremiumSpaceComponent implements AfterViewInit {
+  currentSlide: number = 0;
+  slides: HTMLElement[] = [];
 
+  ngAfterViewInit() {
+    // Pegue todos os slides (divs com a classe 'image-slide') após a visualização estar pronta
+    this.slides = Array.from(document.querySelectorAll('.image-slide')) as HTMLElement[];
+
+    // Comece a transição automática
+    this.startSlideshow();
+  }
+
+  startSlideshow() {
+    setInterval(() => {
+      this.changeSlide();
+    }, 5000); // Troca de slide a cada 5 segundos
+  }
+
+  changeSlide() {
+    // Torna o slide atual invisível
+    this.slides[this.currentSlide].style.opacity = '0';
+
+    // Atualiza o índice para o próximo slide
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+
+    // Torna o próximo slide visível
+    this.slides[this.currentSlide].style.opacity = '1';
+  }
 }
